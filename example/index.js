b0098e8f17ce528b0ec6ddee6a999b959064d7f3
Vladimir Mandic <mandic00@live.com>
2021-02-20 21:49:39 -0500

    update

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@ import * as faceapi from '../dist/face-api.esm.js';
 // configuration options
 const modelPath = 'https://vladmandic.github.io/face-api/model/'; // path to model folder that will be loaded using http
 // const modelPath = '../model/'; // path to model folder that will be loaded using http
-const imgSize = 512; // maximum image size in pixels
+const imgSize = 720; // maximum image size in pixels
 const minScore = 0.1; // minimum score
 const maxResults = 10; // maximum number of results to return
 const samples = ['sample (1).jpg', 'sample (2).jpg', 'sample (3).jpg', 'sample (4).jpg', 'sample (5).jpg', 'sample (6).jpg']; // sample images to be loaded using http
--- example/node-singleprocess.js
+++ example/node-singleprocess.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 
 const fs = require('fs');
+const process = require('process');
 const path = require('path');
 const log = require('@vladmandic/pilogger');
 // eslint-disable-next-line import/no-extraneous-dependencies, node/no-unpublished-require
@@ -11,6 +12,7 @@ const modelPathRoot = '../model';
 const imgPathRoot = './example'; // modify to include your sample images
 const minScore = 0.1;
 const maxResults = 5;
+let optionsSSDMobileNet;
 
 async function image(img) {
   const buffer = fs.readFileSync(img);
@@ -22,10 +24,19 @@ async function image(img) {
   return result;
 }
 
+async function detect(tensor) {
+  const result = await faceapi
+    .detectAllFaces(tensor, optionsSSDMobileNet)
+    .withFaceLandmarks()
+    .withFaceExpressions()
+    .withFaceDescriptors()
+    .withAgeAndGender();
+  return result;
+}
+
 async function main() {
   log.header();
   log.info('FaceAPI single-process test');
-  const t0 = process.hrtime.bigint();
 
   await faceapi.tf.setBackend('tensorflow');
   await faceapi.tf.enableProdMode();
@@ -41,23 +52,32 @@ async function main() {
   await faceapi.nets.faceLandmark68Net.loadFromDisk(modelPath);
   await faceapi.nets.faceRecognitionNet.loadFromDisk(modelPath);
   await faceapi.nets.faceExpressionNet.loadFromDisk(modelPath);
-  const optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({ minConfidence: minScore, maxResults });
+  optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({ minConfidence: minScore, maxResults });
 
-  const dir = fs.readdirSync(imgPathRoot);
-  for (const img of dir) {
-    if (!img.toLocaleLowerCase().endsWith('.jpg')) continue;
-    const tensor = await image(path.join(imgPathRoot, img));
-    const result = await faceapi
-      .detectAllFaces(tensor, optionsSSDMobileNet)
-      .withFaceLandmarks()
-      .withFaceExpressions()
-      .withFaceDescriptors()
-      .withAgeAndGender();
-    log.data('Image:', img, 'Detected faces:', result.length);
-    tensor.dispose();
+  if (process.argv.length !== 3) {
+    const t0 = process.hrtime.bigint();
+    const dir = fs.readdirSync(imgPathRoot);
+    for (const img of dir) {
+      if (!img.toLocaleLowerCase().endsWith('.jpg')) continue;
+      const tensor = await image(path.join(imgPathRoot, img));
+      const result = await detect(tensor);
+      log.data('Image:', img, 'Detected faces:', result.length);
+      tensor.dispose();
+    }
+    const t1 = process.hrtime.bigint();
+    log.info('Processed', dir.length, 'images in', Math.trunc(parseInt(t1 - t0) / 1000 / 1000), 'ms');
+  } else {
+    const param = process.argv[2];
+    if (fs.existsSync(param)) {
+      const tensor = await image(param);
+      const result = await detect(tensor);
+      log.data('Image:', param, 'Detected faces:', result.length);
+      for (const i of result) {
+        log.data('Gender:', i.genderProbability, i.gender, 'Age:', i.age);
+      }
+      tensor.dispose();
+    }
   }
-  const t1 = process.hrtime.bigint();
-  log.info('Processed', dir.length, 'images in', Math.trunc(parseInt(t1 - t0) / 1000 / 1000), 'ms');
 }
 
 main();

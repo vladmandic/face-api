b12488feaf476cb71b85d89caf876d94c647d885
Bettina Steger <hello@bettysteger.com>
2021-05-25 17:57:04 +0200

    fixes for mov videos

--- src/dom/bufferToVideo.ts
+++ src/dom/bufferToVideo.ts
@@ -3,20 +3,16 @@ import { env } from '../env/index';
 export function bufferToVideo(buf: Blob): Promise<HTMLVideoElement> {
   return new Promise((resolve, reject) => {
     if (!(buf instanceof Blob)) reject(new Error('bufferToVideo - expected buf to be of type: Blob'));
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (typeof reader.result !== 'string') reject(new Error('bufferToVideo - expected reader.result to be a string, in onload'));
-      const video = env.getEnv().createVideoElement();
-      video.onloadstart = () => {
-        setTimeout(() => resolve(video), 100);
-      };
-      video.onerror = reject;
-      // video.type = 'video/mp4';
-      video.autoplay = true;
-      video.muted = true;
-      video.src = reader.result as string;
+
+    const video = env.getEnv().createVideoElement();
+    video.onloadstart = () => {
+      setTimeout(() => resolve(video), 100);
     };
-    reader.onerror = reject;
-    reader.readAsDataURL(buf);
+    video.onerror = reject;
+    video.type = buf.type;
+    video.playsInline = true;
+    video.autoplay = true;
+    video.muted = true;
+    video.src = URL.createObjectURL(buf);
   });
 }

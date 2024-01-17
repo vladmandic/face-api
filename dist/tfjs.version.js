438897c5a2f47f8bc46104b9b62dc2ee57e85b01
Vladimir Mandic <mandic00@live.com>
2024-01-17 16:44:24 -0500

    update all dependencies

--- dist/tfjs.version.js
+++ dist/tfjs.version.js
@@ -4,4 +4,34 @@
   author: <https://github.com/vladmandic>'
 */
 
-var e="4.7.0";var s="4.7.0";var t="4.7.0";var n="4.7.0";var i="4.7.0";var w={tfjs:e,"tfjs-core":e,"tfjs-converter":s,"tfjs-backend-cpu":t,"tfjs-backend-webgl":n,"tfjs-backend-wasm":i};export{w as version};
+
+// node_modules/.pnpm/@tensorflow+tfjs-core@4.16.0/node_modules/@tensorflow/tfjs-core/package.json
+var version = "4.16.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-converter@4.16.0_@tensorflow+tfjs-core@4.16.0/node_modules/@tensorflow/tfjs-converter/package.json
+var version2 = "4.16.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-backend-cpu@4.16.0_@tensorflow+tfjs-core@4.16.0/node_modules/@tensorflow/tfjs-backend-cpu/package.json
+var version3 = "4.16.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-backend-webgl@4.16.0_@tensorflow+tfjs-core@4.16.0/node_modules/@tensorflow/tfjs-backend-webgl/package.json
+var version4 = "4.16.0";
+
+// node_modules/.pnpm/@tensorflow+tfjs-backend-wasm@4.16.0_@tensorflow+tfjs-core@4.16.0/node_modules/@tensorflow/tfjs-backend-wasm/package.json
+var version5 = "4.16.0";
+
+// src/tfjs/tf-version.ts
+var version6 = {
+  // tfjs: tfjsVersion,
+  tfjs: version,
+  "tfjs-core": version,
+  // 'tfjs-data': tfjsDataVersion,
+  // 'tfjs-layers': tfjsLayersVersion,
+  "tfjs-converter": version2,
+  "tfjs-backend-cpu": version3,
+  "tfjs-backend-webgl": version4,
+  "tfjs-backend-wasm": version5
+};
+export {
+  version6 as version
+};
